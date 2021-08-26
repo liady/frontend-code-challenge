3db1195a39d09a7813589f8f84bcba70b6721889
Igor <igor-dv@users.noreply.github.com>
2021-08-26 18:32:42 +0300

    feat(challenge): add editorconfig

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import { App } from './components/app.container';
 import { store } from './store';
 
 ReactDOM.render(
-  <React.StrictMode>
-      <Provider store={store}>
-    <App />
-      </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
